4c333323ad1ef949ac8d27c643574f209a102164
CorruptDirectory <corruptdirectory@proton.me>
2024-07-01 12:03:03 +0300

    Added HelloSerializer

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+# Django ootab, et APIView tagastaks Response objekti
+
+class HelloApiView(APIView):
+    """TestAPI View
+    You define a url (endpoint),
+     then you assign this to a view,
+      and djr handles it by calling appropriate
+       function in a view for HTTP request that you made.
+    """
+    
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as funtion (get, post, patch, put, delete)',
+            'Is similar to traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs',
+        ]
+        return Response({'message':'Cnock cnock...', 'an_apiview': an_apiview})
